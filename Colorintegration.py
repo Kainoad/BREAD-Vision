c67bf67a880d4070dc8d474080a04887bf9fa8f2
Kainoad <developer2334@gmail.com>
2023-02-28 16:21:06 -0500

    Combine color integration and face detect

--- Colorintegration.py
+++ Colorintegration.py
@@ -1,26 +1,22 @@
 import cv2
-from numpy import *
-import time
-from picamera import PiCamera
+# from numpy import *
+import numpy as np
+
 #initialize tolarance specs
-required=array([10,10,10])
-utolarance=array([0,0,0])
-ltolarance=array([0,0,0])
-utolarance=required+10
-ltolarance=required-10
+required = np.array([10,10,10])
+utolarance = np.array([0,0,0])
+ltolarance = np.array([0,0,0])
+utolarance = required+10
+ltolarance = required-10
 #image frame
 height = 720
 width = 1280
-camera = PiCamera()
-camera.resolution = (1280, 720)
-time.sleep(2)
-#for loop to index color until told to stop.
-while(True):
-    camera.capture("img.jpg")
-    image = cv2.imread("img.jpg")
-    color=array(image[int(height/2),int(width/2)])
-    print(color)
-    if all(ltolarance<color) and all(utolarance>color):
-        print("True")
-    else:
-        print("False")
+
+class ColorIntegration:
+    def process_image(image):
+        color = np.array(image[int(height/2),int(width/2)])
+        # print(color)
+        if all(ltolarance<color) and all(utolarance>color):
+            return str(color) + "True"
+        else:
+            return str(color) + "False"
---
+++ main.py
@@ -0,0 +1,28 @@
+import cv2
+from numpy import *
+import time
+# from picamera import PiCamera
+
+from Colorintegration import ColorIntegration
+from detect_and_track import DetectAndTrack
+
+#image frame
+height = 720
+width = 1280
+camera = PiCamera()
+camera.resolution = (width, height)
+time.sleep(2)
+#for loop to index color until told to stop.
+while(True):
+    camera.capture("img.jpg")
+    image = cv2.imread("img.jpg")
+    
+    print("Color Intregration:")
+    print(ColorIntegration.process_image(image))
+
+    print("Detect And Track:")
+    print(DetectAndTrack.process_image(image))
+
+    time.sleep(2)
+
+
