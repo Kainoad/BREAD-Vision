7949a4f982135459929a95802785fc4defd82814
Kainoad <developer2334@gmail.com>
2023-04-26 22:11:28 -0400

    Code to upload to OSF

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from numpy import *
 import time
 from picamera2 import Picamera2
 
+import sys
+import smbus2 as smbus
+
+# Slave Addresses
+I2C_SLAVE_ADDRESS = 11
+
 from Colorintegration import ColorIntegration
 from detect_and_track import DetectAndTrack
 
@@ -21,20 +27,77 @@ camera.start()
 # camera.resolution = (width, height)
 
 time.sleep(2)
-#for loop to index color until told to stop.
-while(True):
-    # camera.capture("img.jpg")
+# #for loop to index color until told to stop.
+# while(True):
+    # # camera.capture("img.jpg")
+    # # image = cv2.imread("img.jpg")
+    # # image = picam2.switch_mode_and_capture_image(capture_config)
+    # camera.capture_file("img.jpg")
     # image = cv2.imread("img.jpg")
-    # image = picam2.switch_mode_and_capture_image(capture_config)
-    camera.capture_file("img.jpg")
-    image = cv2.imread("img.jpg")
     
-    print("Color Intregration:")
-    print(ColorIntegration.process_image(image))
+    # print("Color Intregration:")
+    # print(ColorIntegration.process_image(image))
 
-    print("Detect And Track:")
-    print(DetectAndTrack.process_image(image))
+    # print("Detect And Track:")
+    # print(DetectAndTrack.process_image(image))
 
-    time.sleep(2)
+    # time.sleep(2)
+    
+    # break
 
 
+
+# This function converts a string to an array of bytes.
+def ConvertStringsToBytes(src):
+  converted = []
+  for b in src:
+    converted.append(ord(b))
+  return converted
+
+
+def main(args):
+    I2Cbus = smbus.SMBus(1)
+    
+    BytesToSend = 0;
+    #for loop to index color until told to stop.
+    while(True):
+        # # cmd = input("Enter command: ")
+        
+        # # BytesToSend = ConvertStringsToBytes(cmd)
+        # BytesToSend += 1;
+        # print("\nSent BREAD Loaf the " + str(BytesToSend) + " command.")
+        # # print(BytesToSend)
+        # # I2Cbus.write_i2c_block_data(I2C_SLAVE_ADDRESS, 0x00, BytesToSend)
+        # I2Cbus.write_byte_data(I2C_SLAVE_ADDRESS, 0x00, BytesToSend)
+        # time.sleep(0.5)
+        
+        # try:
+            # # data = I2Cbus.read_i2c_block_data(I2C_SLAVE_ADDRESS,0x00,2)
+            # data = I2Cbus.read_byte_data(I2C_SLAVE_ADDRESS, 0)
+            # print("recieve from BREAD Loaf:", data)
+            
+        # except:
+            # print("remote i/o error")
+            # time.sleep(0.5)
+        
+        
+        camera.capture_file("img.jpg")
+        image = cv2.imread("img.jpg")
+        
+        print("Color Intregration:")
+        print(ColorIntegration.process_image(image))
+
+        print("Detect And Track:")
+        print(DetectAndTrack.process_image(image))
+
+        time.sleep(1)
+            
+    return 0
+
+
+if __name__ == '__main__':
+     try:
+        main(sys.argv)
+     except KeyboardInterrupt:
+        print("program was stopped manually")
+     input()
